f3b238dbdbe9dc7598a9e473617af310ddfea9f7
Lucifer <143842404+ProttushD@users.noreply.github.com>
2026-06-01 17:46:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+const navbar = document.querySelector(".nav-bottom");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
+
+
+
+
+
+const cards = document.querySelector(".cards");
+const card = document.querySelector(".card");
+const left = document.querySelector(".c-left");
+const right = document.querySelector(".c-right");
+
+let currentSlide = 0;
+const maxSlide = 7;
+
+function moveSlider(){
+
+    const cardWidth = card.offsetWidth;
+    const gap = 32;
+
+    let moveAmount;
+
+    if(window.innerWidth <= 768){
+        moveAmount = (cardWidth / 2) + gap;
+    } else{
+        moveAmount = cardWidth + gap;
+    }
+
+    cards.style.transform = `translateX(-${currentSlide * moveAmount}px)`;
+}
+
+right.onclick = function () {
+    if(currentSlide < maxSlide){
+        currentSlide++;
+        moveSlider();
+    }
+};
+
+left.onclick = function () {
+    if(currentSlide > 0){
+        currentSlide--;
+        moveSlider();
+    }
+};
+
+window.addEventListener("resize", moveSlider);
+
+
+
+
+const reviewCards = document.querySelector(".review-cards");
+const reviewCard = document.querySelector(".review-card");
+const revLeft = document.querySelector(".rev-left");
+const revRight = document.querySelector(".rev-right");
+
+let reviewSlide = 0;
+const reviewMaxSlide = 2;
+
+function moveReviewSlider(){
+    const cardWidth = reviewCard.offsetWidth;
+    const gap = 32;
+
+    reviewCards.style.transform =
+        `translateX(-${reviewSlide * (cardWidth + gap)}px)`;
+}
+
+revRight.onclick = function () {
+    if(reviewSlide < reviewMaxSlide){
+        reviewSlide++;
+        moveReviewSlider();
+    }
+};
+
+revLeft.onclick = function () {
+    if(reviewSlide > 0){
+        reviewSlide--;
+        moveReviewSlider();
+    }
+};
+
+window.addEventListener("resize", moveReviewSlider);
